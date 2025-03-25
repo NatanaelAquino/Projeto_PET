2727d13ea853fa6fdfebc655646572f7d703cd4c
NatanaelAquino <wedit62@gmail.com>
2025-03-25 17:17:14 -0300

    Atualização no backend e dependências do projeto

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-
+const UserRouter = require('./router/UserRoutes')
 const app = express()
 
 //config JSON response 
@@ -14,6 +14,6 @@ app.use(cors({credentials: true , origin: 'http://localhost:3000'}))
 app.use(express.static('public'))
 
 //Routes 
-
+app.use('/users', UserRouter)
 
 app.listen(5000)
